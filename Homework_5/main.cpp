205148170d49817124a801714bbc7af832ce8882
Teldenes <83219067+Teldenes@users.noreply.github.com>
2022-04-30 01:12:51 +0200

    Added walls

--- Homework_5/main.cpp
+++ Homework_5/main.cpp
@@ -3,70 +3,10 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-class CustomRectangleShape : public sf::RectangleShape
- {
-  public:
-    CustomRectangleShape(const sf::Vector2f &size, const sf::Vector2f &position) : sf::RectangleShape(size)
-     {
-      setPosition(position);
-     }
-
-    void setSpeed(int xspeed, int yspeed, int rspeed)
-     {
-      m_speed_x = xspeed;
-      m_speed_y = yspeed;
-      m_speed_r = rspeed;
-     }
-
-    void setBounds(int left, int right, int top, int bottom)
-     {
-      bound_top = top;
-      bound_bottom = bottom;
-      bound_right = right;
-      bound_left = left;
-     }
-
-    void moveInDirection(const sf::Time &elapsed, const sf::Keyboard::Key &key)
-     {
-      sf::FloatRect rectangle_bounds = getGlobalBounds();
-
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && rectangle_bounds.top > bound_top)
-       {
-        move(0, -m_speed_y*elapsed.asSeconds());
-       }
-
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && rectangle_bounds.top+rectangle_bounds.height < bound_bottom)
-       {
-        move(0, m_speed_y*elapsed.asSeconds());
-       }
-
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && rectangle_bounds.left + rectangle_bounds.width < bound_right)
-       {
-        move(m_speed_x*elapsed.asSeconds(), 0);
-       }
-
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && rectangle_bounds.left > bound_left)
-       {
-        move(-m_speed_x*elapsed.asSeconds(), 0);
-       }
-     }
-
-
-  private:
-     int m_speed_x = 4185;
-     int m_speed_y = 5271;
-     int m_speed_r = 121;
-     int bound_top = 0;
-     int bound_bottom = 0;
-     int bound_left = 0;
-     int bound_right = 0;
-//     bool selected = 0;
- };
 
 class CustomSprite : public sf::Sprite
  {
   public:
-//    CustomSprite();
 
     void setSpeed(int xspeed, int yspeed)
      {
@@ -108,7 +48,6 @@ class CustomSprite : public sf::Sprite
      }
 
 
-
   private:
     int m_speed_x = 200;
     int m_speed_y = 200;
@@ -131,8 +70,64 @@ int main()
         return 1;
     }
 
+    sf::Texture grass_tex;
+    if (!grass_tex.loadFromFile("grass.png")) {
+        std::cerr << "Could not load texture" << std::endl;
+        return 1;
+    }
+    grass_tex.setRepeated(true);
+
+    sf::Texture wall_tex;
+    if (!wall_tex.loadFromFile("wall.png")) {
+        std::cerr << "Could not load texture" << std::endl;
+        return 1;
+    }
+    wall_tex.setRepeated(true);
+
+
     CustomSprite guy;
     guy.setTexture(guy_tex);
+
+    sf::Sprite grass;
+    grass.setTexture(grass_tex);
+    grass.setScale(1, 1);
+    grass.setTextureRect(sf::IntRect(0, 0, window.getSize().x, window.getSize().y));
+
+    sf::Sprite wall1;
+    wall1.setTexture(wall_tex);
+    wall1.setScale(0.3, 0.3);
+    wall1.setTextureRect(sf::IntRect(0, 0, 100, 900));
+    wall1.setPosition(200, 100);
+
+    sf::Sprite wall2;
+    wall2.setTexture(wall_tex);
+    wall2.setScale(0.3, 0.3);
+    wall2.setTextureRect(sf::IntRect(0, 0, 700, 100));
+    wall2.setPosition(230, 250);
+
+    sf::Sprite wall3;
+    wall3.setTexture(wall_tex);
+    wall3.setScale(0.3, 0.3);
+    wall3.setTextureRect(sf::IntRect(0, 0, 100, 1100));
+    wall3.setPosition(440, 220);
+
+    sf::Sprite wall4;
+    wall4.setTexture(wall_tex);
+    wall4.setScale(0.3, 0.3);
+    wall4.setTextureRect(sf::IntRect(0, 0, 100, 1000));
+    wall4.setPosition(570, 110);
+
+    sf::Sprite wall5;
+    wall5.setTexture(wall_tex);
+    wall5.setScale(0.3, 0.3);
+    wall5.setTextureRect(sf::IntRect(0, 0, 600, 100));
+    wall5.setPosition(570, 410);
+
+    sf::Sprite wall6;
+    wall6.setTexture(wall_tex);
+    wall6.setScale(0.3, 0.3);
+    wall6.setTextureRect(sf::IntRect(0, 0, 700, 100));
+    wall6.setPosition(100, 460);
 
     guy.setBounds(0, window.getSize().x, 0, window.getSize().y);
 
@@ -153,11 +148,14 @@ int main()
        }
 
       window.clear(sf::Color::Black);
+      window.draw(grass);
       window.draw(guy);
-
-
-
-
+      window.draw(wall1);
+      window.draw(wall2);
+      window.draw(wall3);
+      window.draw(wall4);
+      window.draw(wall5);
+      window.draw(wall6);
 
       guy.moveInDirection(elapsed, event.key.code);
 
